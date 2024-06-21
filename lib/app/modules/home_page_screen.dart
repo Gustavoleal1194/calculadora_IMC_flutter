a8499907bcde23a5f05dda53614bb7da23b26b1a
Gustavo Leal <gustavo.leal@cronosbank.com.br>
2024-06-21 16:40:29 -0300

    added responsivity with mediaQuery

--- lib/app/modules/home_page_screen.dart
+++ lib/app/modules/home_page_screen.dart
@@ -28,7 +28,7 @@ class _MyHomePageState extends State<MyHomePage> {
       final double imc = imcRequirido.calcularImc();
 
       _resultadoController.text =
-          'olá $nome, seu IMC é ${imc.toStringAsFixed(2)}';
+          'olá $nome, seu IMC é ${imc.toStringAsFixed(2)}\n\n- menor que 16 — magreza grave\n- entre 16 e 16,9 — magreza moderada\n- entre 17 e 18,5 — magreza leve\n- entre 18,6 e 24,9 — peso ideal\n- entre 25 e 29,9 — sobrepeso\n- entre 30 e 34,9 — obesidade grau I\n - entre 35 e 39,9 — obesidade grau II\n- maiores do que 40 — obesidade grau III';
     } else {
       _resultadoController.text = 'por favor insira valores validos';
     }
@@ -56,102 +56,113 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
               Expanded(
                 child: SizedBox(
-                  width: 600,
-                  child: ListView(
-                    children: [
-                      SizedBox(
-                        width: 300,
-                        child: Flexible(
-                          child: TextFormField(
-                            controller: _nomeController,
-                            decoration: InputDecoration(
-                              labelText: 'Digite seu nome',
-                              fillColor: Colors.white,
-                              filled: true,
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(12),
-                                borderSide: const BorderSide(
-                                    color: Colors.grey, width: 2.0),
+                  child: LayoutBuilder(
+                    builder: (context, constraints) {
+                      double widthFactor;
+                      if (constraints.maxWidth > 900) {
+                        widthFactor = 0.35;
+                      } else if (constraints.maxWidth < 500) {
+                        widthFactor = 0.65;
+                      } else {
+                        widthFactor = 0.65;
+                      }
+
+                      return SizedBox(
+                        width: constraints.maxWidth * widthFactor,
+                        child: ListView(
+                          children: [
+                            SizedBox(
+                              child: Flexible(
+                                child: TextFormField(
+                                  controller: _nomeController,
+                                  decoration: InputDecoration(
+                                    labelText: 'Digite seu nome',
+                                    fillColor: Colors.white,
+                                    filled: true,
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(12),
+                                      borderSide: const BorderSide(
+                                          color: Colors.grey, width: 2.0),
+                                    ),
+                                  ),
+                                ),
                               ),
                             ),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      Flexible(
-                        child: SizedBox(
-                          width: 300,
-                          child: TextFormField(
-                            controller: _pesoController,
-                            decoration: InputDecoration(
-                              labelText: 'Digite seu peso',
-                              fillColor: Colors.white,
-                              filled: true,
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(12),
-                                borderSide: const BorderSide(
-                                    color: Colors.grey, width: 2.0),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            Flexible(
+                              child: SizedBox(
+                                child: TextFormField(
+                                  controller: _pesoController,
+                                  decoration: InputDecoration(
+                                    labelText: 'Digite seu peso',
+                                    fillColor: Colors.white,
+                                    filled: true,
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(12),
+                                      borderSide: const BorderSide(
+                                          color: Colors.grey, width: 2.0),
+                                    ),
+                                  ),
+                                ),
                               ),
                             ),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      Flexible(
-                        child: SizedBox(
-                          width: 300,
-                          child: TextFormField(
-                            controller: _alturaController,
-                            decoration: InputDecoration(
-                              labelText: 'digite sua altura',
-                              fillColor: Colors.white,
-                              filled: true,
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(12),
-                                borderSide: const BorderSide(
-                                    color: Colors.grey, width: 2.0),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            Flexible(
+                              child: SizedBox(
+                                child: TextFormField(
+                                  controller: _alturaController,
+                                  decoration: InputDecoration(
+                                    labelText: 'digite sua altura',
+                                    fillColor: Colors.white,
+                                    filled: true,
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(12),
+                                      borderSide: const BorderSide(
+                                          color: Colors.grey, width: 2.0),
+                                    ),
+                                  ),
+                                ),
                               ),
                             ),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      SizedBox(
-                        width: 139,
-                        height: 46,
-                        child: TextButton(
-                          onPressed: () {
-                            calcularIMC();
-                          },
-                          style: const ButtonStyle(
-                            backgroundColor: WidgetStatePropertyAll(
-                              Color(0xff8A19D6),
+                            const SizedBox(
+                              height: 20,
                             ),
-                          ),
-                          child: const Text(
-                            'Calcular',
-                            style: TextStyle(color: Colors.white),
-                          ),
+                            SizedBox(
+                              height: 46,
+                              child: TextButton(
+                                onPressed: () {
+                                  calcularIMC();
+                                },
+                                style: const ButtonStyle(
+                                  backgroundColor: WidgetStatePropertyAll(
+                                    Color(0xff8A19D6),
+                                  ),
+                                ),
+                                child: const Text(
+                                  'Calcular',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 100,
+                            ),
+                            Text(
+                              _resultadoController.text,
+                              style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.bold),
+                              textAlign: TextAlign.center,
+                            ),
+                          ],
                         ),
-                      ),
-                      const SizedBox(
-                        height: 100,
-                      ),
-                      Text(
-                        _resultadoController.text,
-                        style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold),
-                        textAlign: TextAlign.center,
-                      ),
-                    ],
+                      );
+                    },
                   ),
                 ),
               ),
